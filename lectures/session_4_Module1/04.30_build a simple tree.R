78a1f87376340057a41a80ba856f2bc351016ce4
Fernando Carazo <fcarazo@tecnun.es>
2022-02-23 22:10:37 +0100

    session 4

---
+++ lectures/session_4_Module1/04.30_build a simple tree.R
@@ -0,0 +1,11 @@
+# Crear manualmente un arbol con dos niveles en este problema de clasificación binaria
+
+
+
+library(tidyverse)
+x1 <- c(4,-3,3,1,-2,-3)
+x2 <- c(3,-1,-2,4,3,5)
+x3 <- c(-1,-1,0,0,1,5)
+y  <- c(1,0,0,1,0,0)
+datos <- data.frame(x1,x2,x3,y)
+datos
